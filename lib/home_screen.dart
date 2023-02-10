0eb521969cd546b73d3423f1ff01d1191530bd29
ylose <heungeob1003@gmail.com>
2023-02-10 12:55:32 +0900

    구글 지도를 사용하기 위한 gogole_maps_flutter 라이브러리 사용

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
