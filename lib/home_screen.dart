6ad6fa4992d9bb27190cfbc5a60d198247d3f9ad
ylose <heungeob1003@gmail.com>
2023-02-10 12:52:18 +0900

    앱바를 구현하는 함수 renderAppBar 생성

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: renderAppBar(),
       body: Text('Home Screen'),
     );
   }
+}
+
+AppBar renderAppBar() {
+  //AppBar를 구현하는 함수
+  return AppBar(
+    centerTitle:true,
+    title:Text(
+      '오늘도 출근',
+      style:TextStyle(
+        color:Colors.blue,
+        fontWeight:FontWeight.w700,
+      ),
+    ),
+    backgroundColor:Colors.white,
+  );
 }
