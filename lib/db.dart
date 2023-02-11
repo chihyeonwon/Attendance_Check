759ebbe74f760c22295765b7b93134719523a16f
ylose <heungeob1003@gmail.com>
2023-02-11 14:13:23 +0900

    득근득근 앱 프로젝트에서 sqflite 코드 가져오기

---
+++ lib/db.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:io';
+import 'grocery.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'groceries.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE groceries(
+      id INTEGER PRIMARY KEY,
+      name TEXT
+    )
+    ''');
+  }
+
+  Future<int> remove(int id) async {
+    Database db = await instance.database;
+    return await db.delete('groceries', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db.update('groceries', grocery.toMap(),
+        where: 'id = ?', whereArgs: [grocery.id]);
+  }
+
+  Future<List<Grocery>> getGroceries() async {
+    Database db = await instance.database;
+    var groceries = await db.query('groceries', orderBy: 'name');
+    List<Grocery> groceryList = groceries.isNotEmpty
+        ? groceries.map((c) => Grocery.fromMap(c)).toList()
+        : [];
+    return groceryList;
+  }
+
+  Future<int> add(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db.insert('groceries', grocery.toMap());
+  }
+}
