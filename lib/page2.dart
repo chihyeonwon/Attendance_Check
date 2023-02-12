40828721d6d9baff2bea49373c84c5a01dcc6bd5
ylose <heungeob1003@gmail.com>
2023-02-12 15:29:32 +0900

    등교횟수 텍스트 폰트 사이즈 변경

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'home_screen.dart';
+
 class Page2 extends StatefulWidget {
   const Page2({Key? key}) : super(key: key);
 
@@ -12,20 +14,35 @@ class _Page2State extends State<Page2> {
 
   late SharedPreferences prefs;
 
-  Future initPrefs() async {
+  Future getPrefs() async {
     prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
+    final int? counter = prefs.getInt('attendanceCount');
+    return Text(
+      '${counter}',
+      style:TextStyle(fontSize:100.0),
+    );
   }
 
   @override
   void initState() {
     super.initState();
-    initPrefs();
+    getPrefs();
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-    );
+      body:Column(
+        children: [
+          FutureBuilder(
+           future: getPrefs(),
+           builder:(context, snapshot) {
+           return snapshot.data;
+           }
+          ),
+        ],
+      ),
+      );
   }
 }
