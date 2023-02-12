ff5076e9a1c34058e01146c8388aabb16e4b7ad2
ylose <heungeob1003@gmail.com>
2023-02-12 12:29:50 +0900

    FutureBuilder가 들어가 있는 부분을 Column으로 감싸기

--- lib/page2.dart
+++ lib/page2.dart
@@ -22,60 +22,62 @@ class _Page2State extends State<Page2> {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: FutureBuilder<List<Grocery>>(
-          future: DatabaseHelper.instance.getGroceries(),
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Grocery>> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: Text('Loading'),
-              );
-            }
-            return snapshot.data!.isEmpty
-                ? Center(child:Text('오늘 할 운동 목록이 비어있습니다.',style:TextStyle(fontSize:20)),
-            )
-                : ListView(
-              children: snapshot.data!.map((grocery) {
+      body: Column(
+        children: [
+          FutureBuilder<List<Grocery>>(
+            future: DatabaseHelper.instance.getGroceries(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Grocery>> snapshot) {
+              if (!snapshot.hasData) {
                 return Center(
-                  child: Card(
-                    color: selectedId == grocery.id
-                        ? Colors.white70
-                        : Colors.white,
-                    child: ListTile(
-                      onTap: () {
-                        setState(() {
-                          if (selectedId == null) {
-                            textController.text = grocery.name;
-                            selectedId = grocery.id;
-                          } else {
-                            textController.text = '';
-                            selectedId = null;
-                          }
-                        });
-                      },
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(grocery.name),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              setState(() {
-                                DatabaseHelper.instance
-                                    .remove(grocery.id!);
-                              });
-                            },
-                          ),
-                        ],
+                  child: Text('Loading'),
+                );
+              }
+              return snapshot.data!.isEmpty
+                  ? Center(child:Text('오늘 할 운동 목록이 비어있습니다.',style:TextStyle(fontSize:20)),
+              )
+                  : ListView(
+                children: snapshot.data!.map((grocery) {
+                  return Center(
+                    child: Card(
+                      color: selectedId == grocery.id
+                          ? Colors.white70
+                          : Colors.white,
+                      child: ListTile(
+                        onTap: () {
+                          setState(() {
+                            if (selectedId == null) {
+                              textController.text = grocery.name;
+                              selectedId = grocery.id;
+                            } else {
+                              textController.text = '';
+                              selectedId = null;
+                            }
+                          });
+                        },
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(grocery.name),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                setState(() {
+                                  DatabaseHelper.instance
+                                      .remove(grocery.id!);
+                                });
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              }).toList(),
-            );
-          },
-        ),
+                  );
+                }).toList(),
+              );
+            },
+          ),
+        ],
       ),
       floatingActionButton: ElevatedButton(
         onPressed: () async {
