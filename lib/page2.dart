9412aa47ac2914e2b153df8703f1378afb1a6ccc
ylose <heungeob1003@gmail.com>
2023-02-12 13:22:51 +0900

    shared_preferences 라이브러리 사용

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Page2 extends StatefulWidget {
   const Page2({Key? key}) : super(key: key);
