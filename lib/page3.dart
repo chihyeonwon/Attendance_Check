f823326c6150456bfad1d9ccc54d739c9d6b492a
ylose <heungeob1003@gmail.com>
2023-02-13 15:11:46 +0900

    등교 정보 페이지 first commit

---
+++ lib/page3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Page3 extends StatefulWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  State<Page3> createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
