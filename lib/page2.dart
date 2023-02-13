bd9b7141e30a4558a159005bd34d5a47be7e7629
ylose <heungeob1003@gmail.com>
2023-02-13 15:18:07 +0900

    이름,학과 입력 폼과 등교횟수, futureBuilder(이름,학과)를 분리

--- lib/page2.dart
+++ lib/page2.dart
@@ -14,37 +14,6 @@ class _Page2State extends State<Page2> {
 
   late SharedPreferences prefs;
 
-  Future getPrefs() async {
-    prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
-    final int? counter = prefs.getInt('attendanceCount');
-    return Text(
-      '${counter}',
-      style:TextStyle(fontSize:100.0),
-    );
-  }
-
-  Future getName() async {
-    prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
-    final String? name = prefs.getString('name');
-    return Text(name!,
-      style:TextStyle(fontSize:100.0),
-    );
-  }
-
-  Future getDept() async {
-    prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
-    final String? dept = prefs.getString('dept');
-    return Text(dept!,
-      style:TextStyle(fontSize:100.0),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getPrefs();
-  }
-
   final _formKey = GlobalKey<FormState>(); // 폼의 상태를 얻기 위한 키
 
   final _nameController = TextEditingController(); // 이름 컨트롤러 객체
@@ -57,16 +26,6 @@ class _Page2State extends State<Page2> {
       body:SingleChildScrollView(
         child: Column(
           children: [
-            Text(
-              '등교한 횟수 : ',
-              style: TextStyle(fontSize: 50.0),
-            ),
-            FutureBuilder(
-              future: getPrefs(),
-              builder:(context, snapshot) {
-                return snapshot.data;
-              }
-          ),
             Form(
               key:_formKey, // 키 할당
               child: Column(
@@ -116,24 +75,6 @@ class _Page2State extends State<Page2> {
                       child:Text('저장'),
                     ),
                   ),
-                  FutureBuilder(
-                    future:getName(),
-                    builder:(context, snapshot) {
-                      if(snapshot.hasData){
-                        return snapshot.data!;
-                      }
-                      return Text('이름 입력을 진행해주세요.');
-                    }
-                  ),
-                  FutureBuilder(
-                      future:getDept(),
-                      builder:(context, snapshot) {
-                        if(snapshot.hasData) {
-                          return snapshot.data!;
-                        }
-                        return Text('학과 입력을 진행해주세요.');
-                      }
-                      ),
                 ],
               ),
             ),
