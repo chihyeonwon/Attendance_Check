817ae07bd1af1b572a392e3d1a18b6d3e3b439e6
ylose <heungeob1003@gmail.com>
2023-02-13 15:18:15 +0900

    이름,학과 입력 폼과 등교횟수, futureBuilder(이름,학과)를 분리

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Page3 extends StatefulWidget {
   const Page3({Key? key}) : super(key: key);
 
@@ -8,8 +10,81 @@ class Page3 extends StatefulWidget {
 }
 
 class _Page3State extends State<Page3> {
+
+  late SharedPreferences prefs;
+
+  Future getPrefs() async {
+    prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
+    final int? counter = prefs.getInt('attendanceCount');
+    return Text(
+      '${counter}',
+      style:TextStyle(fontSize:100.0),
+    );
+  }
+
+  Future getName() async {
+    prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
+    final String? name = prefs.getString('name');
+    return Text(name!,
+      style:TextStyle(fontSize:100.0),
+    );
+  }
+
+  Future getDept() async {
+    prefs = await SharedPreferences.getInstance(); // 사용자의 저장소에 connection
+    final String? dept = prefs.getString('dept');
+    return Text(dept!,
+      style:TextStyle(fontSize:100.0),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPrefs();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              '등교한 횟수 : ',
+              style: TextStyle(fontSize: 50.0),
+            ),
+            FutureBuilder(
+                future: getPrefs(),
+                builder:(context, snapshot) {
+                  return snapshot.data;
+                }
+            ),
+            Column(
+                children:<Widget>[
+                  FutureBuilder(
+                      future:getName(),
+                      builder:(context, snapshot) {
+                        if(snapshot.hasData){
+                          return snapshot.data!;
+                        }
+                        return Text('이름 입력을 진행해주세요.');
+                      }
+                  ),
+                  FutureBuilder(
+                      future:getDept(),
+                      builder:(context, snapshot) {
+                        if(snapshot.hasData) {
+                          return snapshot.data!;
+                        }
+                        return Text('학과 입력을 진행해주세요.');
+                      }
+                  ),
+                ],
+              ),
+            ],
+        ),
+      ),
+    );
   }
 }
